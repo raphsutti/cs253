325f33d3fb84f207be075a1d7c1ab5aeaf228947
Raph <rsuttiyotin@seek.com.au>
2020-10-09 14:11:45 +1100

    Formatting

--- server6.js
+++ server6.js
@@ -1,30 +1,30 @@
 // XSS
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const express = require('express')
-const { randomBytes } = require('crypto')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const { randomBytes } = require("crypto");
 
-const app = express()
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.set('X-XSS-Protection', '0') 
-  next()
-})
+  res.set("X-XSS-Protection", "0");
+  next();
+});
 
 const USERS = {
-  alice: 'password',
-  bob: 'hunter2'
-}
+  alice: "password",
+  bob: "hunter2",
+};
 const BALANCES = {
   alice: 500,
-  bob: 100
-}
-const SESSIONS = {}
+  bob: 100,
+};
+const SESSIONS = {};
 
-app.get('/', (req, res) => {
-  const username = SESSIONS[req.cookies.sessionId]
-  const source = req.query.source
+app.get("/", (req, res) => {
+  const username = SESSIONS[req.cookies.sessionId];
+  const source = req.query.source;
 
   if (username) {
     res.send(`
@@ -37,13 +37,13 @@ app.get('/', (req, res) => {
         <input type='submit' value='Send' />
       </form>
       <h1>
-      ${source ? `Hi ${source} reader` : ''}
+      ${source ? `Hi ${source} reader` : ""}
       </h1>
-    `)
+    `);
   } else {
     res.send(`
       <h1>
-      ${source ? `Hi ${source} reader` : ''}
+      ${source ? `Hi ${source} reader` : ""}
       Log in to your bank account:
       </h1>
       <form method='POST' action='/login'>
@@ -51,46 +51,45 @@ app.get('/', (req, res) => {
         Password: <input name='password' type='password' />
         <input type='submit' value='Login' />
         </form>
-    `)
+    `);
   }
-})
+});
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body
-  const actualPassword = USERS[username]
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const actualPassword = USERS[username];
   if (req.body.password === actualPassword) {
-    const SessionId = randomBytes(16).toString('base64')
-    SESSIONS [SessionId] = username 
-    res.cookie('sessionId', SessionId);
-    res.redirect('/') 
+    const SessionId = randomBytes(16).toString("base64");
+    SESSIONS[SessionId] = username;
+    res.cookie("sessionId", SessionId);
+    res.redirect("/");
   } else {
-    res.send('failed login')
+    res.send("failed login");
   }
-})
+});
 
-app.get('/logout', (req, res) => {
-  const sessionId = req.cookies.sessionId
-  delete SESSIONS[sessionId]
-  res.clearCookie('sessionId')
-  res.redirect('/')
-})
+app.get("/logout", (req, res) => {
+  const sessionId = req.cookies.sessionId;
+  delete SESSIONS[sessionId];
+  res.clearCookie("sessionId");
+  res.redirect("/");
+});
 
-app.post('/transfer', (req, res) => {
-  const sessionId = req.cookies.sessionId
-  const username = SESSIONS[sessionId]
+app.post("/transfer", (req, res) => {
+  const sessionId = req.cookies.sessionId;
+  const username = SESSIONS[sessionId];
 
   if (!username) {
-    res.send('fail!')
-    return
+    res.send("fail!");
+    return;
   }
-  
-  const amount =  Number(req.body.amount)
-  const to = req.body.to
 
-  BALANCES[username] -= amount
-  BALANCES[to] += amount
-  res.redirect('/')
-})
+  const amount = Number(req.body.amount);
+  const to = req.body.to;
 
-app.listen(4000)
+  BALANCES[username] -= amount;
+  BALANCES[to] += amount;
+  res.redirect("/");
+});
 
+app.listen(4000);
